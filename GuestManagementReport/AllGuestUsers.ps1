cbb74a3074c40ed5351bba05b1f7aac523a92382
Donovan du Val <49915971+Donovand4@users.noreply.github.com>
2022-07-08 15:32:00 +0100

    Update AllGuestUsers.ps1

--- GuestManagementReport/AllGuestUsers.ps1
+++ GuestManagementReport/AllGuestUsers.ps1
@@ -1,9 +1,2 @@
 #App Permissions = User.Read.All
-$TenantID = Get-AutomationVariable -Name 'TenantID'
-$AppID = Get-AutomationVariable -Name 'ServicePrincipalAppID' #app id from app registrations that has been granted access to read the users and manage groups.
-$AuthCert = Get-AutomationCertificate -Name 'cert'
-$connection = Connect-MgGraph -ClientID $AppID -TenantId $TenantID -CertificateThumbprint $AuthCert.Thumbprint
-
-$GuestUsers = Get-MgUser -Filter "userType eq 'Guest'" -All | Select-Object DisplayName, UserPrincipalName, AccountEnabled, mail, CreationType, RefreshTokensValidFromDateTime, UserType, UserState
-
-Write-Output ($GuestUsers | ConvertTo-Json)
+WIP
