5db94cfec41a6ef1067fb5b6c2e1daf7030eb49f
Donovan du Val <49915971+Donovand4@users.noreply.github.com>
2025-02-21 13:24:19 +0000

    Update AllGuestsSigninDates.ps1

--- UserManagement/AllGuestsSigninDates.ps1
+++ UserManagement/AllGuestsSigninDates.ps1
@@ -4,9 +4,8 @@ $TenantID = Get-AutomationVariable -Name 'TenantID'
 $AppID = Get-AutomationVariable -Name 'ServicePrincipalAppID' #app id from app registrations that has been granted access to read the users and manage groups.
 $AuthCert = Get-AutomationCertificate -Name 'cert'
 $connection = Connect-MgGraph -ClientID $AppID -TenantId $TenantID -CertificateThumbprint $AuthCert.Thumbprint
-Select-MgProfile -Name beta
 
-$GuestUsers = Get-MgUser -Filter "userType eq 'Guest'" -All -Property DisplayName, UserPrincipalName, AccountEnabled, mail, CreationType, UserType, ExternalUserState, signInActivity `
+$GuestUsers = Get-MgBetaUser -Filter "userType eq 'Guest'" -All -Property DisplayName, UserPrincipalName, AccountEnabled, mail, CreationType, UserType, ExternalUserState, signInActivity `
  | Select-Object DisplayName, UserPrincipalName, AccountEnabled, mail, CreationType, UserType, ExternalUserState, `
  @{N='LastSignInDateTime';E={$_.signInActivity.LastSignInDateTime.DateTime}}, @{N='LastNonInteractiveSignInDateTime';E={$_.signInActivity.LastNonInteractiveSignInDateTime.DateTime}}
 
